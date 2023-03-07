9f0f3c14dd9477e0b27244d0a91b1f0fca0abb8a
mesh-dell <mwaimonicas@gmail.com>
2023-03-08 02:59:07 +0300

    Functionality 1

--- app/src/main/java/com/example/speechtotext/MainActivity.java
+++ app/src/main/java/com/example/speechtotext/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.speechtotext;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        editText = findViewById(R.id.editText);
+
         setContentView(R.layout.activity_main);
     }
 }
