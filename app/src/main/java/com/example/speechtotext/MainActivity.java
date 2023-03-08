6f19b98aaea4000321b6839d0289a4961d97e23a
mesh-dell <mwaimonicas@gmail.com>
2023-03-09 02:07:11 +0300

    Functionality 2

--- app/src/main/java/com/example/speechtotext/MainActivity.java
+++ app/src/main/java/com/example/speechtotext/MainActivity.java
@@ -1,18 +1,116 @@
 package com.example.speechtotext;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     EditText editText;
+    SpeechRecognizer mSpeechRecogniser;
+    Intent mSpeechRecogniserIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        //checkPermission();
         editText = findViewById(R.id.editText);
+        mSpeechRecogniser = SpeechRecognizer.createSpeechRecognizer(this);
+        mSpeechRecogniserIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        mSpeechRecogniserIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        mSpeechRecogniserIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        mSpeechRecogniser.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int i) {
+
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+                ArrayList<String> matches = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                if(matches!=null)
+                    editText.setText(matches.get(0));
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+
+            }
+        });
+        findViewById(R.id.button).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()){
+                    case MotionEvent.ACTION_UP:
+                        mSpeechRecogniser.stopListening();
+                        editText.setHint("You will see the input here");
+                        break;
+                    case  MotionEvent.ACTION_DOWN:
+                        editText.setText("");
+                        editText.setHint("Listening...");
+                        mSpeechRecogniser.startListening(mSpeechRecogniserIntent);
+                        break;
+
+                }
+                return false;
+            }
+        });
 
         setContentView(R.layout.activity_main);
     }
+    /*private  void checkPermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(!ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO == PackageManager.PERMISSION_GRANTED)){
+                Intent intent = new Intent(Settings.ACTION_APPLICATION_SETTINGS, Uri.parse("package:"+getPackageName()));
+                startActivity(intent);
+                finish();
+            }
+        }
+    }*/
 }
